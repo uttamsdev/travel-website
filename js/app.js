a3c522803ddf65bcf1ecf0b0a452fe0d01bb558a
uttamsaha <uttamsaha982@gmail.com>
2022-07-05 19:31:39 +0600

    updated

---
+++ js/app.js
@@ -0,0 +1,10 @@
+const bars = document.querySelector(".bars");
+const navbar = document.querySelector(".nav-bar");
+bars.onclick = () => {
+  const navBar = document.querySelector(".nav-items");
+  navBar.classList.toggle("active");
+};
+
+window.onscroll = () => {
+    this.scrollY > 1000 ? navbar.classList.add("sticky") : navbar.classList.remove("sticky");
+  };
